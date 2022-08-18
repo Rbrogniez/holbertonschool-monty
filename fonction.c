869478a02226549315b2157c03df318f66881fe6
murdonicolai <murdo.nicolai.pro@gmail.com>
2022-08-18 16:27:43 +0200

    solving leaks

--- fonction.c
+++ fonction.c
@@ -15,26 +15,26 @@ void push(stack_t **stack, unsigned int line_number)
 	int num;
 	stack_t *new;
 	stack_t *tmp;
-	char* word;
+	char *word;
 
 	tmp = *stack;
 	word = strtok(NULL, " \n");
 	if (!word)
 	{
-		fprintf(stderr, "L%d: usage: push integer\n",line_number);
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	num = atoi(word);
 	if (!num && word[0] != '0')
 	{
-		fprintf(stderr, "L%d: usage: push integer\n",line_number);
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	new = malloc(sizeof(stack_t));
 	new->n = num;
 	new->next = tmp;
 	new->prev = NULL;
-	if(tmp)
+	if (tmp)
 		tmp->prev = new;
 	*stack = new;
 }
@@ -52,27 +52,23 @@ void pall(stack_t **stack, unsigned int line_number)
 
 	while (tmp)
 	{
-		printf("%d\n",tmp->n);
+		printf("%d\n", tmp->n);
 		tmp = tmp->next;
 	}
-	if(0)
-		printf("%d",line_number);
+	if (0)
+		printf("%d", line_number);
 
 
 }
 void pint(stack_t **stack, unsigned int line_number)
 {
-	printf("%d%p",line_number,(void *)stack);
+	printf("%d%p", ine_number, (void *)stack);
 }
 void pop(stack_t **stack, unsigned int line_number)
 {
-	printf("%d%p",line_number,(void *)stack);
+	printf("%d%p", line_number, (void *)stack);
 }
 void add(stack_t **stack, unsigned int line_number)
 {
-	printf("%d%p",line_number,(void *)stack);
-}
-void nop(stack_t **stack, unsigned int line_number)
-{
-	printf("%d%p",line_number,(void *)stack);
+	printf("%d%p", line_number, (void *)stack);
 }
--- main.c
+++ main.c
@@ -3,24 +3,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	printf("%d%p", line_number, (void *)stack);
+}
+
+void freeStack(stack_t **stack)
+{
+	stack_t *tmp = *stack;
+
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+		free(tmp->prev);
+	}
+	free(tmp);
+}
+
 void initfonc(instruction_t *structfonc)
 {
-	structfonc[0].opcode = malloc(10);
 	structfonc[0].opcode = "push";
 	structfonc[0].f = push;
-	structfonc[1].opcode = malloc(10);
 	structfonc[1].opcode = "pall";
 	structfonc[1].f = pall;
-	structfonc[2].opcode = malloc(10);
 	structfonc[2].opcode = "pint";
 	structfonc[2].f = pint;
-	structfonc[3].opcode = malloc(sizeof("pop"));
 	structfonc[3].opcode = "pop";
 	structfonc[3].f = pop;
-	structfonc[4].opcode = malloc(10);
 	structfonc[4].opcode = "add";
 	structfonc[4].f = add;
-	structfonc[5].opcode = malloc(10);
 	structfonc[5].opcode = "nop";
 	structfonc[5].f = nop;
 }
@@ -36,7 +48,7 @@ void initfonc(instruction_t *structfonc)
 int main(int ac, char **av)
 {
 	FILE *file;
-	char* word;
+	char *word;
 	int j;
 	char line[256];
 	unsigned int linenum = 0;
@@ -62,10 +74,10 @@ int main(int ac, char **av)
 		for (j = 0; j < 6; j++)
 		{
 			if (word && strcmp(word, structfonc[j].opcode) == 0)
-				structfonc[j].f(&head,linenum);
+				structfonc[j].f(&head, linenum);
 		}
 	}
-
+	freeStack(&head);
 	fclose(file);
 	return (EXIT_SUCCESS);
 }
